2e72aafeaae93aced436ce7d726b1bb43df93c31
Utkarsh Patel <utkarshp@marketamerica.com>
2025-07-23 14:46:10 -0400

    Add OpenAI model client implementation with environment variable loading

---
+++ config/openai_model_client.py
@@ -0,0 +1,18 @@
+# https://microsoft.github.io/autogen/stable//user-guide/agentchat-user-guide/tutorial/models.html#azure-openai
+
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv('OPENAI_API_KEY')
+
+
+def get_model_client():
+    openai_model_client = OpenAIChatCompletionClient(
+        model='gpt-4o',
+        api_key= api_key
+    )
+
+    return openai_model_client
